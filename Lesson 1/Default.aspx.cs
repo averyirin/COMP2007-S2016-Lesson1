ac28a9701cd93b0236b1cb4f2647b8797428ceed
averyirin <averyirin@gmail.com>
2016-05-12 12:34:04 -0400

    Lesson Branch to Master

---
+++ Lesson 1/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lesson_1
+{
+    public partial class Default : System.Web.UI.Page //extends
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            FullNameTextBox.Text = FirstNameTextBox.Text.ToString() + " " + LastNameTextBox.Text.ToString();
+        }
+
+     
+    }
+}
